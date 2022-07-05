9408eadbdd315ad73bafe4bd8fc70f34ed03e802
webss <93769881+webssoffice@users.noreply.github.com>
2022-07-05 13:41:59 +0200

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    
-    require_once 'controllers/controller.php';
-    require_once 'models/crud.php';
-    require_once 'models/routes.php';
-
-    $mvc = new MvcController();
-    $mvc->viewTemplate();
