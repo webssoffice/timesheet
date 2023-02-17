bd5a5c3a1aedc48d3f2fe25f8dd050593ca9f28b
webss <93769881+webssoffice@users.noreply.github.com>
2023-02-17 09:39:11 +0100

    Add files via upload

--- webss_timesheet.sql
+++ webss_timesheet.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.7
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Creato il: Lug 05, 2022 alle 15:10
--- Versione del server: 10.2.44-MariaDB-cll-lve
--- Versione PHP: 7.4.29
+-- Host: 127.0.0.1
+-- Creato il: Feb 17, 2023 alle 09:37
+-- Versione del server: 10.4.27-MariaDB
+-- Versione PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -16,10 +15,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `webss_timesheet`
+-- Database: `test`
 --
 
 -- --------------------------------------------------------
@@ -34,7 +33,7 @@ CREATE TABLE `agenda` (
   `agenda_date` date DEFAULT NULL,
   `agenda_time` time DEFAULT NULL,
   `event` longtext DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -44,21 +43,22 @@ CREATE TABLE `agenda` (
 
 CREATE TABLE `employees` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `email` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `password` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   `level` int(11) NOT NULL,
-  `employee_rate` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `csrf` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `employee_rate` varchar(255) DEFAULT NULL,
+  `csrf` varchar(255) DEFAULT NULL,
   `registration_date` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dump dei dati per la tabella `employees`
 --
 
 INSERT INTO `employees` (`id`, `name`, `email`, `password`, `level`, `employee_rate`, `csrf`, `registration_date`) VALUES
-(1, 'Test Test', 'test@test.test', '$5$5crHBIc6qyFtq66V$OkDJBlTJMbcahaXIbNF.Tcx5is1Cf4jur99W9lupkCD', 1, '25', '$5$5crHBIc6qyFtq66V$OkDJBlTJMbcahaXIbNF.Tcx5is1Cf4jur99W9lupkCD', '2022-07-05 15:05:22');
+(9, 'User', 'user@webss.ro', '$5$5crHBIc6qyFtq66V$xSSdOB3VC9DNocvtkEiFyBJ6ZNeEG2MsrgUq/yV7KzA', 2, '1', '$5$5crHBIc6qyFtq66V$xSSdOB3VC9DNocvtkEiFyBJ6ZNeEG2MsrgUq/yV7KzA', '2023-02-01 18:29:17'),
+(11, 'Admin', 'admin@webss.ro', '$5$5crHBIc6qyFtq66V$bcnu4Qf9xvN6qU1ktGKliED3/D/eNESZkD4a9oTUac/', 1, '1', '$5$5crHBIc6qyFtq66V$bcnu4Qf9xvN6qU1ktGKliED3/D/eNESZkD4a9oTUac/', '2023-02-17 10:30:55');
 
 -- --------------------------------------------------------
 
@@ -68,9 +68,9 @@ INSERT INTO `employees` (`id`, `name`, `email`, `password`, `level`, `employee_r
 
 CREATE TABLE `projects` (
   `id` int(11) NOT NULL,
-  `project` mediumtext CHARACTER SET utf8 DEFAULT NULL,
+  `project` mediumtext DEFAULT NULL,
   `details` longtext DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -85,8 +85,11 @@ CREATE TABLE `works` (
   `start_time` datetime DEFAULT NULL,
   `end_time` datetime DEFAULT NULL,
   `comment` longtext DEFAULT NULL,
-  `paid` varchar(255) NOT NULL DEFAULT '0'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `rate` varchar(255) DEFAULT NULL,
+  `paid` varchar(255) NOT NULL DEFAULT '0',
+  `edit_by` int(11) DEFAULT NULL,
+  `last_edit` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Indici per le tabelle scaricate
@@ -130,7 +133,7 @@ ALTER TABLE `agenda`
 -- AUTO_INCREMENT per la tabella `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT per la tabella `projects`
